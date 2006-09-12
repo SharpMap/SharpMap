1c6a43de67077a57a38b2f4db14ff18f1d97e2b9
codekaizen <codekaizen@gmail.com>
2006-09-12 19:48:10 +0000

    Branching Test
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C5634

---
+++ ExampleProject/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
