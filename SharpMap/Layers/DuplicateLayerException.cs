23cc4c95f841402d59bb24c57424b60fd789bf0b
codekaizen <codekaizen@gmail.com>
2007-08-16 06:40:25 +0000

    Adding DuplicateLayerException Adding comments to LayerCollection and implementing IndexOf
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C25708

---
+++ SharpMap/Layers/DuplicateLayerException.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SharpMap.Layers
+{
+    /// <summary>
+    /// Exception thrown when a layer with a name which is the same
+    /// as an existing layer is added to a <see cref="LayerCollection"/>.
+    /// </summary>
+    [Serializable]
+    public class DuplicateLayerException : InvalidOperationException
+    {
+        private readonly string _duplicateLayerName;
+
+        /// <summary>
+        /// Creates a new instance of DuplicateLayerException, indicating
+        /// the duplicate layer name by <paramref name="duplicateLayerName"/>.
+        /// </summary>
+        /// <param name="duplicateLayerName">
+        /// The existing layer name which was duplicated.
+        /// </param>
+        public DuplicateLayerException(string duplicateLayerName)
+            : this(duplicateLayerName, null) { }
+
+        /// <summary>
+        /// Creates a new instance of DuplicateLayerException, indicating
+        /// the duplicate layer name by <paramref name="duplicateLayerName"/>
+        /// and including a message.
+        /// </summary>
+        /// <param name="duplicateLayerName">
+        /// The existing layer name which was duplicated.
+        /// </param>
+        /// <param name="message">Additional information about the exception.</param>
+        public DuplicateLayerException(string duplicateLayerName, string message)
+            : this(duplicateLayerName, message, null) { }
+
+        /// <summary>
+        /// Creates a new instance of DuplicateLayerException, indicating
+        /// the duplicate layer name by <paramref name="duplicateLayerName"/>
+        /// and including a message.
+        /// </summary>
+        /// <param name="duplicateLayerName">
+        /// The existing layer name which was duplicated.
+        /// </param>
+        /// <param name="message">
+        /// Additional information about the exception.
+        /// </param>
+        /// <param name="inner">
+        /// An exception which caused this exception, if any.
+        /// </param>
+        public DuplicateLayerException(string duplicateLayerName, string message, Exception inner)
+            : base(message, inner)
+        {
+            _duplicateLayerName = duplicateLayerName;
+        }
+
+        /// <summary>
+        /// Creates a new instance of DuplicateLayerException from serialized data,
+        /// <paramref name="info"/>.
+        /// </summary>
+        /// <param name="info">The serialization data.</param>
+        /// <param name="context">Serialzation context.</param>
+        protected DuplicateLayerException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            _duplicateLayerName = info.GetString("_duplicateLayerName");
+        }
+
+        /// <summary>
+        /// Gets the existing layer name which was duplicated.
+        /// </summary>
+        public string DuplicateLayerName
+        {
+            get { return _duplicateLayerName; }
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("_duplicateLayerName", _duplicateLayerName);
+        }
+    }
+}
--- SharpMap/Layers/LayerCollection.cs
+++ SharpMap/Layers/LayerCollection.cs
@@ -22,60 +22,152 @@ using SharpMap.Layers;
 
 namespace SharpMap.Layers
 {
-	public class LayerCollection : CollectionBase
-	{
-		
-	    public void Add(ILayer layer)
-	    { 
-	    	List.Add(layer);
-	    }
-	
-	    public new void RemoveAt(int index)
-	    {
-	        List.RemoveAt(index);
-	    }	
-	    
-	    public void Remove(ILayer layer) 
-	    {
-			List.Remove(layer);
-	    }
-	    
-	    public void Insert(int index, ILayer layer) 
-	    {
-	        if ((index > (Count - 1)) || (index < 0))
-	            throw (new Exception("Index not valid for LayerCollection!"));
-	        List.Insert(index, layer);
-	    }    
-		
-	    public virtual ILayer this[int index]
-	    {
-	    	get { return (ILayer) this.List[index]; }
-	    	set { this.List[index] = value; }
-	    }    
-	    
-	    public virtual ILayer this[string LayerName]
-	    {
-	    	get { return this.GetLayerByName(LayerName); }
-	    	set { ILayer layer = this.GetLayerByName(LayerName);
-	    		  layer = value; }
-	    }    
-	    
-	    private ILayer GetLayerByName(string LayerName)
-	    {
-	    	for(int i=0;i<List.Count;i++)
-	    		if((List[i] as ILayer).LayerName == LayerName)
-	    			return (List[i] as ILayer);
-	    	
-	    	return null;
-	    }
-	    
-	    protected override void OnInsert(int index, object value)
-		{
-	    	ILayer newLayer = (value as ILayer);
-	    	
-			for(int i=0;i<List.Count;i++)
-				if(String.Equals(newLayer.LayerName, (List[i] as ILayer).LayerName))
-					throw new Exception("This layer name already exists. Layer name: " + newLayer.LayerName);
-	    }
-	}
+    /// <summary>
+    /// A collection of <see cref="ILayer"/> instances.
+    /// </summary>
+    public class LayerCollection : CollectionBase
+    {
+        /// <summary>
+        /// Adds a layer to the collection.
+        /// </summary>
+        /// <param name="layer">The layer to add.</param>
+        public void Add(ILayer layer)
+        {
+            List.Add(layer);
+        }
+        
+        /// <summary>
+        /// Removes the layer at the given index.
+        /// </summary>
+        /// <param name="index">
+        /// The index at which to remove the layer.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="index"/> is less than 0. 
+        /// or is equal to or greater than <see cref="Count"/>.
+        /// </exception>
+        public new void RemoveAt(int index)
+        {
+            List.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Removes the given layer. No effect if <paramref name="layer"/>
+        /// is not in the collection.
+        /// </summary>
+        /// <param name="layer">The layer to remove. May be null.</param>
+        public void Remove(ILayer layer)
+        {
+            List.Remove(layer);
+        }
+
+        /// <summary>
+        /// Returns the index of the given <paramref name="layer"/>,
+        /// or -1 if the layer doesn't exist in the collection.
+        /// </summary>
+        /// <param name="layer">The layer to remove.</param>
+        /// <returns>
+        /// The index of the given <paramref name="layer"/>,
+        /// or -1 if the layer doesn't exist in the collection.
+        /// </returns>
+        public int IndexOf(ILayer layer)
+        {
+            return List.IndexOf(layer);
+        }
+
+        /// <summary>
+        /// Inserts the layer at the given <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">The index at which to add the layer.</param>
+        /// <param name="layer">The layer to insert.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if <paramref name="index"/> is less than 0 or is 
+        /// greater or equal to <see cref="Count"/>.
+        /// </exception>
+        public void Insert(int index, ILayer layer)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index", index, "Index not in range");
+            }
+
+            List.Insert(index, layer);
+        }
+
+        /// <summary>
+        /// Gets or sets the layer at the given <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">Index of the layer to get or set.</param>
+        /// <returns>The layer at the given <paramref name="index"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown if <paramref name="index"/> is less than 0 or is 
+        /// greater or equal to <see cref="Count"/>.
+        /// </exception>
+        public virtual ILayer this[int index]
+        {
+            get { return (ILayer)List[index]; }
+            set { List[index] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the layer with the given <paramref name="layerName"/>.
+        /// </summary>
+        /// <param name="layerName">
+        /// Name of the layer to replace, if it exists.
+        /// </param>
+        public virtual ILayer this[string layerName]
+        {
+            get { return getLayerByName(layerName); }
+            set
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    int comparison = String.Compare((List[i] as ILayer).LayerName, 
+                        layerName, StringComparison.CurrentCultureIgnoreCase);
+
+                    if (comparison == 0)
+                    {
+                        List[i] = value;
+                        return;
+                    }
+                }
+
+                Add(value);
+            }
+        }
+
+        protected override void OnInsert(int index, object value)
+        {
+            ILayer newLayer = (value as ILayer);
+
+            foreach (ILayer layer in List)
+            {
+                int comparison = String.Compare(layer.LayerName,
+                    newLayer.LayerName, StringComparison.CurrentCultureIgnoreCase);
+
+                if (comparison == 0)
+                {
+                    throw new DuplicateLayerException(newLayer.LayerName);
+                }
+            }
+
+            base.OnInsert(index, value);
+        }
+
+        private ILayer getLayerByName(string layerName)
+        {
+            foreach (ILayer layer in List)
+            {
+                int comparison = String.Compare(layer.LayerName,
+                    layerName, StringComparison.CurrentCultureIgnoreCase);
+
+                if (comparison == 0)
+                {
+                    return layer;
+                }
+            }
+
+            return null;
+        }
+    }
 }
