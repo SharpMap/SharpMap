06ce5041e85d858e1a8c04a138f1e77a2b8bbc12
volleyknaller <volleyknaller@unknown.com>
2007-11-28 19:46:40 +0000

    Updated the Ogr - Layer from SharpMap.Extensions Now it is based on Gdal 1.5.0 all required unmanaged binaries are available at:http://www.codeplex.com/SharpMap/Wiki/View.aspx?title=Extensions Download gdal_managed.zip.00* (3 files) and put it into your path or bin folder. Look at sample website, there is an example for MapInfo files.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C28871

--- DemoWebSite/App_Code/MapHelper.cs
+++ DemoWebSite/App_Code/MapHelper.cs
@@ -213,4 +213,104 @@ public class MapHelper
 			HttpContext.Current.Trace.Write("Map initialized");
 			return map;
 		}
+
+    public static SharpMap.Map InitializeMapOgr(System.Drawing.Size size)
+    {
+        HttpContext.Current.Trace.Write("Initializing map...");
+
+        //Initialize a new map of size 'imagesize'
+        SharpMap.Map map = new SharpMap.Map(size);
+
+        //Set up the countries layer
+        SharpMap.Layers.VectorLayer layCountries = new SharpMap.Layers.VectorLayer("Countries");
+        //Set the datasource to a shapefile in the App_data folder
+        try
+        {
+            layCountries.DataSource = new SharpMap.Data.Providers.Ogr(HttpContext.Current.Server.MapPath(@"~\App_data\MapInfo\countriesMapInfo.tab"));
+        }
+        catch (Exception ex)
+        {
+            if (ex.GetType() == typeof(TypeInitializationException))
+                throw new Exception("Please copy the umanaged dll's into your bin folder from javascript:window.location.href='http://www.codeplex.com/SharpMap/Wiki/View.aspx?title=Extensions';.");
+        }
+
+        //Set fill-style to green
+        layCountries.Style.Fill = new SolidBrush(Color.Green);
+        //Set the polygons to have a black outline
+        layCountries.Style.Outline = System.Drawing.Pens.Black;
+        layCountries.Style.EnableOutline = true;
+        layCountries.SRID = 4326;
+
+        //Set up a river layer
+        SharpMap.Layers.VectorLayer layRivers = new SharpMap.Layers.VectorLayer("Rivers");
+        //Set the datasource to a shapefile in the App_data folder
+        layRivers.DataSource = new SharpMap.Data.Providers.Ogr(HttpContext.Current.Server.MapPath(@"~\App_data\MapInfo\riversMapInfo.tab"));
+        //Define a blue 1px wide pen
+        layRivers.Style.Line = new Pen(Color.Blue, 1);
+        layRivers.SRID = 4326;
+
+        //Set up a river layer
+        SharpMap.Layers.VectorLayer layCities = new SharpMap.Layers.VectorLayer("Cities");
+        //Set the datasource to a shapefile in the App_data folder
+        layCities.DataSource = new SharpMap.Data.Providers.Ogr(HttpContext.Current.Server.MapPath(@"~\App_data\MapInfo\citiesMapInfo.tab"));
+        //Define a blue 1px wide pen
+        //layCities.Style.Symbol = new Bitmap(HttpContext.Current.Server.MapPath(@"~\App_data\icon.png"));
+        layCities.Style.SymbolScale = 0.8f;
+        layCities.MaxVisible = 40;
+        layCities.SRID = 4326;
+
+        //Set up a country label layer
+        SharpMap.Layers.LabelLayer layLabel = new SharpMap.Layers.LabelLayer("Country labels");
+        layLabel.DataSource = layCountries.DataSource;
+        layLabel.Enabled = true;
+        layLabel.LabelColumn = "Name";
+        layLabel.Style = new SharpMap.Styles.LabelStyle();
+        layLabel.Style.ForeColor = Color.White;
+        layLabel.Style.Font = new Font(FontFamily.GenericSerif, 12);
+        layLabel.Style.BackColor = new System.Drawing.SolidBrush(Color.FromArgb(128, 255, 0, 0));
+        layLabel.MaxVisible = 90;
+        layLabel.MinVisible = 30;
+        layLabel.Style.HorizontalAlignment = SharpMap.Styles.LabelStyle.HorizontalAlignmentEnum.Center;
+        layLabel.SRID = 4326;
+        layLabel.MultipartGeometryBehaviour = SharpMap.Layers.LabelLayer.MultipartGeometryBehaviourEnum.Largest;
+
+        //Set up a city label layer
+        SharpMap.Layers.LabelLayer layCityLabel = new SharpMap.Layers.LabelLayer("City labels");
+        layCityLabel.DataSource = layCities.DataSource;
+        layCityLabel.Enabled = true;
+        layCityLabel.LabelColumn = "Name";
+        layCityLabel.Style = new SharpMap.Styles.LabelStyle();
+        layCityLabel.Style.ForeColor = Color.Black;
+        layCityLabel.Style.Font = new Font(FontFamily.GenericSerif, 11);
+        layCityLabel.MaxVisible = layLabel.MinVisible;
+        layCityLabel.Style.HorizontalAlignment = SharpMap.Styles.LabelStyle.HorizontalAlignmentEnum.Left;
+        layCityLabel.Style.VerticalAlignment = SharpMap.Styles.LabelStyle.VerticalAlignmentEnum.Bottom;
+        layCityLabel.Style.Offset = new PointF(3, 3);
+        layCityLabel.Style.Halo = new Pen(Color.Yellow, 2);
+        layCityLabel.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+        layCityLabel.SmoothingMode = SmoothingMode.AntiAlias;
+        layCityLabel.SRID = 4326;
+        layCityLabel.LabelFilter = SharpMap.Rendering.LabelCollisionDetection.ThoroughCollisionDetection;
+        layCityLabel.Style.CollisionDetection = true;
+
+        //Add the layers to the map object.
+        //The order we add them in are the order they are drawn, so we add the rivers last to put them on top
+        map.Layers.Add(layCountries);
+        map.Layers.Add(layRivers);
+        map.Layers.Add(layCities);
+        map.Layers.Add(layLabel);
+        map.Layers.Add(layCityLabel);
+
+
+        //limit the zoom to 360 degrees width
+        map.MaximumZoom = 360;
+        map.BackColor = Color.LightBlue;
+
+        map.ZoomToExtents(); // = 360;
+        map.Center = new SharpMap.Geometries.Point(0, 0);
+
+        HttpContext.Current.Trace.Write("Map initialized");
+        return map;
+    }
+
 }
