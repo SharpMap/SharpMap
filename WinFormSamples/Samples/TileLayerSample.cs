5ac04bcd1f879389d1ad32f143571aa062017c5a
pauldendulk <pauldendulk@gmail.com>
2010-02-13 11:25:12 +0000

    added TileLayerSample.cs
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C63955

---
+++ WinFormSamples/Samples/TileLayerSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpMap.Layers;
+using BruTile;
+using BruTile.Web;
+using SharpMap;
+using SharpMap.Geometries;
+
+namespace WinFormSamples.Samples
+{
+    class TileLayerSample
+    {
+        public static Map InitializeMap()
+        {
+            Map map = new Map();
+            TileLayer tileLayer = new TileLayer(new TileSourceOsm(), "OSM");
+            map.Layers.Add(tileLayer);
+            map.ZoomToBox(tileLayer.Envelope);
+            return map;
+        }
+    }
+}
