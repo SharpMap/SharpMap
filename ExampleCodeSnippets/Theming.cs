5f0d019a99d6d7089847e76994c5f155dae10cd7
FObermaier <felix.obermaier@netcologne.de>
2010-06-02 16:13:17 +0000

    Added sample for picking Symbol from FeatureDataRow
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C66374

--- ExampleCodeSnippets/Theming.cs
+++ ExampleCodeSnippets/Theming.cs
@@ -1,4 +1,3 @@
-
 namespace ExampleCodeSnippets
 {
     public class SymbolRotationTheming
@@ -69,6 +68,78 @@ namespace ExampleCodeSnippets
         }
     }
 
+    public class SymbolFromFeatureDataRowTheming
+    {
+        /// <summary>
+        /// Name of the column, which contains the rotation angle in degrees [0°, 360°]
+        /// </summary>
+        public System.String SymbolColumn { get; set; }
+
+        /// <summary>
+        /// Default vector style from which parts are to be modified
+        /// </summary>
+        public SharpMap.Styles.VectorStyle DefaultStyle { get; set; }
+
+        /// <summary>
+        /// Creates an instance of this class
+        /// </summary>
+        /// <param name="symbolColumn">Name of the column, which contains the symbol</param>
+        public SymbolFromFeatureDataRowTheming(System.String symbolColumn)
+            : this(symbolColumn, new SharpMap.Styles.VectorStyle())
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of this class
+        /// </summary>
+        /// <param name="styleRotationColumn">Name of the column, which contains the symbol</param>
+        /// <param name="defaultStyle">Default vector style from which parts are to be modified</param>
+        public SymbolFromFeatureDataRowTheming(System.String styleRotationColumn, SharpMap.Styles.VectorStyle defaultStyle)
+        {
+            SymbolColumn = styleRotationColumn;
+            DefaultStyle = defaultStyle;
+        }
+
+        private static SharpMap.Styles.VectorStyle CloneStyle(SharpMap.Styles.VectorStyle styleToClone)
+        {
+            SharpMap.Styles.VectorStyle style =
+                new SharpMap.Styles.VectorStyle
+                {
+                    Enabled = styleToClone.Enabled,
+                    MinVisible = styleToClone.MinVisible,
+                    MaxVisible = styleToClone.MaxVisible,
+                    Line = styleToClone.Line.Clone() as System.Drawing.Pen,
+                    Fill = styleToClone.Fill.Clone() as System.Drawing.Brush,
+                    Outline = styleToClone.Outline.Clone() as System.Drawing.Pen,
+                    EnableOutline = styleToClone.EnableOutline,
+                    Symbol = styleToClone.Symbol.Clone() as System.Drawing.Bitmap,
+                    SymbolOffset = styleToClone.SymbolOffset,
+                    SymbolRotation = styleToClone.SymbolRotation,
+                    SymbolScale = styleToClone.SymbolScale
+                };
+
+            return style;
+        }
+
+        public SharpMap.Styles.VectorStyle GetRotatedSymol(SharpMap.Data.FeatureDataRow row)
+        {
+            if (!System.String.IsNullOrEmpty(SymbolColumn))
+                try
+                {
+                    SharpMap.Styles.VectorStyle dataStyle = CloneStyle(DefaultStyle);
+                    if (!row.IsNull(SymbolColumn))
+                    {
+                        dataStyle.Symbol = new System.Drawing.Bitmap(
+                            new System.IO.MemoryStream((byte[]) row[SymbolColumn]));
+                    }
+                    return dataStyle;
+                }
+                catch { }
+
+            return null;
+        }
+    }
+
     [NUnit.Framework.TestFixture]
     public class SymbolRotationThemingTest
     {
@@ -119,6 +190,98 @@ namespace ExampleCodeSnippets
             map.ZoomToExtents();
             System.Drawing.Image mapImage = map.GetMap();
             mapImage.Save("SymbolRotation.bmp");
+        }
+    }
+
+    [NUnit.Framework.TestFixture]
+    public class SymbolFromFeatureDataRowTest
+    {
+        private static readonly System.Random _randomNumberGenerator = new System.Random();
+        static System.Double[] GetRandomOrdinates(System.Int32 size, System.Double min, System.Double max)
+        {
+            System.Double[] arr = new System.Double[size];
+            System.Double width = max - min;
+            for (System.Int32 i = 0; i < size; i++)
+            {
+                System.Double randomValue = _randomNumberGenerator.NextDouble();
+                arr[i] = min + randomValue * width;
+            }
+            return arr;
+        }
+
+        [NUnit.Framework.Test]
+        public void TestSymbolSelectionTheming()
+        {
+            //Create a map
+            SharpMap.Map map = new SharpMap.Map(new System.Drawing.Size(720, 360));
+
+            //Create some random sample data
+            CreatingData cd = new CreatingData();
+            SharpMap.Data.FeatureDataTable fdt =
+                cd.CreatePointFeatureDataTableFromArrays(GetRandomOrdinates(80, -180, 180),
+                                                         GetRandomOrdinates(80, -90, 90), null);
+
+            //Add rotation column and fill with random rotation values
+            fdt.Columns.Add("Symbol", typeof(System.Byte[]));
+            foreach (SharpMap.Data.FeatureDataRow row in fdt.Rows)
+                row["Symbol"] = RandomSymbol((int)System.Math.Floor(_randomNumberGenerator.NextDouble() * 360d));
+
+
+            //Create layer and datasource
+            SharpMap.Layers.VectorLayer vl = new SharpMap.Layers.VectorLayer("Points", new SharpMap.Data.Providers.GeometryFeatureProvider(fdt));
+
+            //Create default style
+            SharpMap.Styles.VectorStyle defaultStyle = new SharpMap.Styles.VectorStyle();
+            defaultStyle.Symbol = new System.Drawing.Bitmap(@"..\..\..\DemoWinForm\Resources\flag.png");
+            defaultStyle.SymbolScale = 0.5f;
+
+            //Create theming class and apply to layer
+            SymbolFromFeatureDataRowTheming srt = new SymbolFromFeatureDataRowTheming("Symbol", defaultStyle);
+            vl.Theme = new SharpMap.Rendering.Thematics.CustomTheme(srt.GetRotatedSymol);
+
+            map.Layers.Add(vl);
+            map.ZoomToExtents();
+            System.Drawing.Image mapImage = map.GetMap();
+            mapImage.Save("SymbolFromFDR.bmp");
+        }
+
+        private byte[] RandomSymbol(int number)
+        {
+            number = number%360;
+            string text ="";
+            System.Drawing.Brush brush = null;
+            if (number < 60)
+                return null;
+            if (number < 120)
+            {
+                text = "<120";
+                brush = System.Drawing.Brushes.DarkGreen;
+            }
+            else if(number < 240)
+            {
+                text = number.ToString();
+                brush = System.Drawing.Brushes.Orange;
+            }
+            else
+            {
+                text = ">240";
+                brush = System.Drawing.Brushes.Red;
+            }
+
+            System.Drawing.Bitmap bmp = new System.Drawing.Bitmap(120, 60);
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bmp);
+            System.Drawing.RectangleF size = new System.Drawing.RectangleF(0f, 0f, 120f, 60f);
+            g.FillRectangle(System.Drawing.Brushes.White, size);
+            var sf = new System.Drawing.StringFormat(System.Drawing.StringFormatFlags.NoWrap)
+                         {Alignment = System.Drawing.StringAlignment.Center};
+
+            g.DrawString(text, new System.Drawing.Font("Arial", 24), brush, size, sf);
+            g.Flush();
+            g.Dispose();
+            var ms = new System.IO.MemoryStream();
+            bmp.MakeTransparent(System.Drawing.Color.White);
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            return ms.ToArray();
         }
     }
 
