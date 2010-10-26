72b87d7dfdf2c2ee2ea107f86bb848d0673ff99e
FObermaier <felix.obermaier@netcologne.de>
2010-10-26 09:53:23 +0000

    [Trunk] - Transformed coordinates in xls before creating XlsLayer, therefore no need to set CoordinateTransfromation.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C78963

--- WinFormSamples/Samples/TileLayerSample.cs
+++ WinFormSamples/Samples/TileLayerSample.cs
@@ -18,26 +18,26 @@ namespace WinFormSamples.Samples
         {
             switch (_num++ % 10)
             {
-                case 0:
-                    return InitializeMapOsm();
                 case 1:
-                    return InitializeMapBing(BingMapType.Roads);
+                    return InitializeMapOsm();
                 case 2:
-                    return InitializeMapBing(BingMapType.Aerial);
+                    return InitializeMapBing(BingMapType.Roads);
                 case 3:
-                    return InitializeMapBing(BingMapType.Hybrid);
+                    return InitializeMapBing(BingMapType.Aerial);
                 case 4:
-                    return InitializeMapGoogle(GoogleMapType.GoogleMap);
+                    return InitializeMapBing(BingMapType.Hybrid);
                 case 5:
-                    return InitializeMapGoogle(GoogleMapType.GoogleSatellite);
+                    return InitializeMapGoogle(GoogleMapType.GoogleMap);
                 case 6:
-                    return InitializeMapGoogle(GoogleMapType.GoogleSatellite | GoogleMapType.GoogleLabels);
+                    return InitializeMapGoogle(GoogleMapType.GoogleSatellite);
                 case 7:
-                    return InitializeMapGoogle(GoogleMapType.GoogleTerrain);
+                    return InitializeMapGoogle(GoogleMapType.GoogleSatellite | GoogleMapType.GoogleLabels);
                 case 8:
-                    return InitializeMapGoogle(GoogleMapType.GoogleLabels);
+                    return InitializeMapGoogle(GoogleMapType.GoogleTerrain);
                 case 9:
                     _num = 0;
+                    return InitializeMapGoogle(GoogleMapType.GoogleLabels);
+                case 0:
                     return InitializeMapOsmWithXls(angle);
 
             }
@@ -72,6 +72,21 @@ namespace WinFormSamples.Samples
                     da.Fill(ds);
             }
 
+            //The SRS for this datasource is EPSG:4326, therefore we need to transfrom it to OSM projection
+            var ctf = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
+            var cf = new ProjNet.CoordinateSystems.CoordinateSystemFactory();
+            var epsg4326 = cf.CreateFromWkt("GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]]");
+            var epsg3785 = cf.CreateFromWkt("PROJCS[\"Popular Visualisation CRS / Mercator\", GEOGCS[\"Popular Visualisation CRS\", DATUM[\"Popular Visualisation Datum\", SPHEROID[\"Popular Visualisation Sphere\", 6378137, 0, AUTHORITY[\"EPSG\",\"7059\"]], TOWGS84[0, 0, 0, 0, 0, 0, 0], AUTHORITY[\"EPSG\",\"6055\"]],PRIMEM[\"Greenwich\", 0, AUTHORITY[\"EPSG\", \"8901\"]], UNIT[\"degree\", 0.0174532925199433, AUTHORITY[\"EPSG\", \"9102\"]], AXIS[\"E\", EAST], AXIS[\"N\", NORTH], AUTHORITY[\"EPSG\",\"4055\"]], PROJECTION[\"Mercator\"], PARAMETER[\"False_Easting\", 0], PARAMETER[\"False_Northing\", 0], PARAMETER[\"Central_Meridian\", 0], PARAMETER[\"Latitude_of_origin\", 0], UNIT[\"metre\", 1, AUTHORITY[\"EPSG\", \"9001\"]], AXIS[\"East\", EAST], AXIS[\"North\", NORTH], AUTHORITY[\"EPSG\",\"3785\"]]");
+            var ct = ctf.CreateFromCoordinateSystems(epsg4326, epsg3785);
+            foreach (System.Data.DataRow row in ds.Tables[0].Rows)
+            {
+                if (row["X"] == DBNull.Value || row["Y"] == DBNull.Value) continue;
+                double[] coords = new double[] { Convert.ToDouble(row["X"]), Convert.ToDouble(row["Y"])};
+                coords = ct.MathTransform.Transform(coords);
+                row["X"] = coords[0];
+                row["Y"] = coords[1];
+            }
+
             //Add Rotation Column
             ds.Tables[0].Columns.Add("Rotation", typeof (float));
             foreach (System.Data.DataRow row in ds.Tables[0].Rows)
@@ -82,13 +97,6 @@ namespace WinFormSamples.Samples
             var xlsLayer = new SharpMap.Layers.VectorLayer("XLS", xlsProvider);
             xlsLayer.Style.Symbol = SharpMap.Styles.VectorStyle.DefaultSymbol;
             
-            //The SRS for this datasource is EPSG:4326, therefore we need to transfrom it to OSM projection
-            var ctf = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
-            var cf = new ProjNet.CoordinateSystems.CoordinateSystemFactory();
-            var epsg4326 = cf.CreateFromWkt("GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]]");
-            var epsg3785 = cf.CreateFromWkt("PROJCS[\"Popular Visualisation CRS / Mercator\", GEOGCS[\"Popular Visualisation CRS\", DATUM[\"Popular Visualisation Datum\", SPHEROID[\"Popular Visualisation Sphere\", 6378137, 0, AUTHORITY[\"EPSG\",\"7059\"]], TOWGS84[0, 0, 0, 0, 0, 0, 0], AUTHORITY[\"EPSG\",\"6055\"]],PRIMEM[\"Greenwich\", 0, AUTHORITY[\"EPSG\", \"8901\"]], UNIT[\"degree\", 0.0174532925199433, AUTHORITY[\"EPSG\", \"9102\"]], AXIS[\"E\", EAST], AXIS[\"N\", NORTH], AUTHORITY[\"EPSG\",\"4055\"]], PROJECTION[\"Mercator\"], PARAMETER[\"False_Easting\", 0], PARAMETER[\"False_Northing\", 0], PARAMETER[\"Central_Meridian\", 0], PARAMETER[\"Latitude_of_origin\", 0], UNIT[\"metre\", 1, AUTHORITY[\"EPSG\", \"9001\"]], AXIS[\"East\", EAST], AXIS[\"North\", NORTH], AUTHORITY[\"EPSG\",\"3785\"]]");
-            xlsLayer.CoordinateTransformation = ctf.CreateFromCoordinateSystems(epsg4326, epsg3785);
-
             //Add layer to map
             map.Layers.Add(xlsLayer);
             var xlsLabelLayer = new SharpMap.Layers.LabelLayer("XLSLabel");
@@ -98,7 +106,6 @@ namespace WinFormSamples.Samples
             xlsLabelLayer.Style.CollisionBuffer = new System.Drawing.SizeF(2f, 2f);
             xlsLabelLayer.Style.CollisionDetection = true;
             xlsLabelLayer.LabelFilter = SharpMap.Rendering.LabelCollisionDetection.ThoroughCollisionDetection;
-            xlsLabelLayer.CoordinateTransformation = xlsLayer.CoordinateTransformation;
             map.Layers.Add(xlsLabelLayer);
 
             map.ZoomToBox(tileLayer.Envelope);
