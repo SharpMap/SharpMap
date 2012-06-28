61571326775e7b88fb5648686360d8806864d0cb
petlof <peter@lofas.se>
2012-06-28 07:49:13 +0000

    Tests for managed sqlite + testdata
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C98518

---
+++ UnitTests/Data/Providers/ManagedSQLiteTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+
+namespace UnitTests.Data.Providers
+{
+    [TestFixture]
+    public class ManagedSQLiteTests
+    {
+        private string GetTestDBPath()
+        {
+            return "Data Source=" + Path.Combine(Path.GetDirectoryName(this.GetType().Assembly.CodeBase.Replace("file:///", "")), @"TestData\test-2.3.sqlite");
+        }
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            GeoAPI.GeometryServiceProvider.Instance = new NetTopologySuite.NtsGeometryServices();
+        }
+
+        [Test]
+        public void TestReadPoints()
+        {
+            SharpMap.Data.Providers.ManagedSpatiaLite sq = new SharpMap.Data.Providers.ManagedSpatiaLite(GetTestDBPath(), "towns", "Geometry", "ROWID");
+            var ext = sq.GetExtents();
+            var ds = new SharpMap.Data.FeatureDataSet();
+            sq.ExecuteIntersectionQuery(ext, ds);
+            Assert.AreEqual(8101, ds.Tables[0].Count);
+
+        }
+        [Test]
+        public void TestReadLines()
+        {
+            SharpMap.Data.Providers.ManagedSpatiaLite sq = new SharpMap.Data.Providers.ManagedSpatiaLite(GetTestDBPath(), "highways", "Geometry", "ROWID");
+            var ext = sq.GetExtents();
+            var ds = new SharpMap.Data.FeatureDataSet();
+            sq.ExecuteIntersectionQuery(ext, ds);
+            Assert.AreEqual(775, ds.Tables[0].Count);
+
+        }
+
+        [Test]
+        public void TestReadPolys()
+        {
+            SharpMap.Data.Providers.ManagedSpatiaLite sq = new SharpMap.Data.Providers.ManagedSpatiaLite(GetTestDBPath(), "regions", "Geometry", "ROWID");
+            var ext = sq.GetExtents();
+            var ds = new SharpMap.Data.FeatureDataSet();
+            sq.ExecuteIntersectionQuery(ext, ds);
+            Assert.AreEqual(109, ds.Tables[0].Count);
+
+        }
+    }
+}
