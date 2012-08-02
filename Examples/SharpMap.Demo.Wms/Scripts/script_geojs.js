37bf074e7fc04e0ccbb8c0f6a2799f5f40bbfd94
DGuidi <diegoguidi@gmail.com>
2012-08-02 07:20:30 +0000

    simplified openlayers samples due to new configuration facilities in OL2.12
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C98997

--- Examples/SharpMap.Demo.Wms/Scripts/script_geojs.js
+++ Examples/SharpMap.Demo.Wms/Scripts/script_geojs.js
@@ -11,40 +11,8 @@ $(document).ready(function () {
 
     options = {
         wms: 'WMS',
-        controls: [],
-        maxExtent: new OpenLayers.Bounds(-2.003750834E7, -2.003750834E7, 2.003750834E7, 2.003750834E7),
-        resolutions: [
-            156543.03390625,
-            78271.516953125,
-            39135.7584765625,
-            19567.87923828125,
-            9783.939619140625,
-            4891.9698095703125,
-            2445.9849047851562,
-            1222.9924523925781,
-            611.4962261962891,
-            305.74811309814453,
-            152.87405654907226,
-            76.43702827453613,
-            38.218514137268066,
-            19.109257068634033,
-            9.554628534317017,
-            4.777314267158508,
-            2.388657133579254,
-            1.194328566789627,
-            0.5971642833948135,
-            0.29858214169740677,
-            0.14929107084870338,
-            0.07464553542435169,
-            0.037322767712175846,
-            0.018661383856087923,
-            0.009330691928043961,
-            0.004665345964021981
-        ],
-        numZoomLevels: 24,
-        projection: new OpenLayers.Projection('EPSG:900913'),
-        displayProjection: new OpenLayers.Projection('EPSG:4326'),
-        units: 'meters',
+        projection: 'EPSG:900913',
+        displayProjection: 'EPSG:4326',
         format: 'text/json'
     };
 
@@ -54,21 +22,14 @@ $(document).ready(function () {
         var zoom = 10;
         var map, url, layers = [], center, highlight;
 
-        map = new OpenLayers.Map('map', options);
-        map.addControl(new OpenLayers.Control.LayerSwitcher());
-        map.addControl(new OpenLayers.Control.NavToolbar());
-        map.addControl(new OpenLayers.Control.PanZoom({
-            position: new OpenLayers.Pixel(2, 10)
-        }));
-        map.addControl(new OpenLayers.Control.MousePosition());
-        map.addControl(new OpenLayers.Control.LoadingPanel());
+        map = new OpenLayers.Map('map', options);        
         map.addLayer(new OpenLayers.Layer.OSM());
 
         url = [
             '/wms.ashx',
             '?SERVICE=', options.wms,
             '&FORMAT=', options.format,
-            '&CRS=', options.projection.getCode(),
+            '&CRS=', options.projection,
             '&REQUEST=GETFEATUREINFO&VERSION=1.3.0&STYLES=&WIDTH=20&HEIGHT=20&I=10&J=10&MAP_TYPE=OL&LAYERS=poi,tiger_roads,poly_landmarks',
             '&FEATURE_COUNT=0', // NOTE: ignored for json requests
             '&INFO_FORMAT=', options.format
@@ -113,7 +74,14 @@ $(document).ready(function () {
             hover: true,
             highlightOnly: true,
             renderIntent: 'temporary'
-        });
+        });        
+        map.addControl(new OpenLayers.Control.LayerSwitcher());
+        map.addControl(new OpenLayers.Control.NavToolbar());
+        map.addControl(new OpenLayers.Control.PanZoom({
+            position: new OpenLayers.Pixel(2, 10)
+        }));
+        map.addControl(new OpenLayers.Control.MousePosition());
+        map.addControl(new OpenLayers.Control.LoadingPanel());
         map.addControl(highlight);
         highlight.activate();
 
