845704181b2e06f800dae5c2503191928e8aa383
FObermaier <felix.obermaier@netcologne.de>
2012-08-03 07:50:24 +0000

    [Trunk] - Code comments
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C99013

--- SharpMap/Rendering/Decoration/IMapDecoration.cs
+++ SharpMap/Rendering/Decoration/IMapDecoration.cs
@@ -2,8 +2,16 @@ using System.Drawing;
 
 namespace SharpMap.Rendering.Decoration
 {
+    /// <summary>
+    /// Interface for all map decorations
+    /// </summary>
     public interface IMapDecoration
     {
+        /// <summary>
+        /// Renders the map decoration to the graphics object
+        /// </summary>
+        /// <param name="g">The graphics object</param>
+        /// <param name="map">The map</param>
         void Render(Graphics g, Map map);
     }
 }
