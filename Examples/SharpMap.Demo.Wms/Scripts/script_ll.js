8e6c2f45b2f605bd732377e1fe496ce187936e39
DGuidi <diegoguidi@gmail.com>
2012-08-06 07:34:00 +0000

    updated samples to leaflet 0.4.2
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C99044

--- Examples/SharpMap.Demo.Wms/Scripts/script_ll.js
+++ Examples/SharpMap.Demo.Wms/Scripts/script_ll.js
@@ -1,14 +1,12 @@
 ﻿$(document).ready(function () {
-    var lon = -73.9529;
-    var lat = 40.7723;
-    var zoom = 10;
+    var lon = -73.9529, lat = 40.7723, zoom = 10,
+        map = new L.Map('map'), center, url, cloudmade;
 
-    var map = new L.Map('map'), center, url, cloudmade;
     map.on('load', function (e) {
-        var bounds, url, options, layer, type;
+        var bounds, jsurl, options, layer;
         bounds = e.target.getBounds();
-        url = [
-           '/json.ashx?MAP_TYPE=PM&BBOX=',
+        jsurl = [
+            '/json.ashx?MAP_TYPE=PM&BBOX=',
             bounds._southWest.lng, ',',
             bounds._southWest.lat, ',',
             bounds._northEast.lng, ',',
@@ -24,31 +22,30 @@
             fillOpacity: 0.8
         };
         layer = new L.GeoJSON(null, {
-            pointToLayer: function (p) {
-                return new L.CircleMarker(p, options);
+            pointToLayer: function (f) {
+                var raw = f.geometry.coordinates;
+                var ll = new L.LatLng(raw[1], raw[0]);
+                return new L.CircleMarker(ll, options);
+            },
+            style: function (f) {
+                var geom = f.geometry, type = geom.type;
+                if (type === 'Polygon' || type === 'MultiPolygon') {
+                    return {
+                        color: 'rgb(0,0,180)',
+                        weight: 4,
+                        opacity: 0.6
+                    };
+                } else if (type === 'LineString' || type === 'MultiLineString') {
+                    return {
+                        color: 'rgb(180,0,0)',
+                        weight: 1,
+                        opacity: 0.9
+                    };
+                }
             }
         });
-        layer.on('featureparse', function (e) {
-            type = e.geometryType;
-            if (type === 'Polygon' || type === 'MultiPolygon') {
-                e.layer.setStyle({
-                    color: 'rgb(0,0,180)',
-                    weight: 4,
-                    opacity: 0.6
-                });
-            }
-            else if (type === 'LineString' || type === 'MultiLineString') {
-                e.layer.setStyle({
-                    color: 'rgb(180,0,0)',
-                    weight: 1,
-                    opacity: 0.9
-                });
-            }
-        });
-        $.getJSON(url, function (e) {
-            $.each(e.features, function () {
-                layer.addGeoJSON(this);
-            });
+        $.getJSON(jsurl, function (evt) {
+            layer.addData(evt.features);
             map.addLayer(layer);
         });
     });
@@ -58,32 +55,10 @@
 
     url = ['http://{s}tile.cloudmade.com', '/1a235b638b614b458deeb77c7dae4f80', '/998/256/{z}/{x}/{y}.png'].join('');
     cloudmade = new L.TileLayer(url, {
-         maxZoom: 18,
-         subdomains: ['a.', 'b.', 'c.', '']
+        maxZoom: 18,
+        subdomains: ['a.', 'b.', 'c.', '']
     });
     map.addLayer(cloudmade);
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
