7dbcc52da1e59afbc207219a1951a8de7990ca54
FObermaier <felix.obermaier@netcologne.de>
2012-12-19 13:33:49 +0000

    [Trunk] - Added SharpMap.Extensions.x86 project with Oracle and SpatiaLite providers
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C100923

---
+++ SharpMap.Extensions.x86/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+
+[assembly: AssemblyTitle("SharpMap Extensions")]
+[assembly: AssemblyDescription("Extensions for the SharpMap Engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("iter.dk")]
+[assembly: AssemblyProduct("SharpMap.Extensions.x86")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2012 Christian Gräfe, SharpMap-Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+
+[assembly: Guid("5b14fbdb-f4fc-498d-9bd9-5d79830311f8")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
+// übernehmen, indem Sie "*" eingeben:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
