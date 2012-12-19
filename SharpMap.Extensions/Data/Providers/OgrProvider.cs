75f4c9f57419d9e036949b7abd0ec308e2e7e1c8
FObermaier <felix.obermaier@netcologne.de>
2012-12-19 20:24:08 +0000

    [Trunk] - Fixing SharpMapServer solution - removed svn:ignore flags and added GdalConfiguration.cs
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C100937

--- SharpMap.Extensions/Data/Providers/OgrProvider.cs
+++ SharpMap.Extensions/Data/Providers/OgrProvider.cs
@@ -74,7 +74,9 @@ namespace SharpMap.Data.Providers
         /// </summary>
         public static string FWToolsVersion
         {
+#pragma warning disable 612,618
             get { return FwToolsHelper.FwToolsVersion; }
+#pragma warning restore 612,618
         }
 
         /// <summary>
--- SharpMapServer/Global.asax.cs
+++ SharpMapServer/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
-using System.Data.Entity;
 using SharpMapServer.Model;
 using SharpMap.Layers;
 using System.IO;
