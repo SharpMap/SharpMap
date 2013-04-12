d2b66bcfca8019b932b2dd3b0a700a5ea5bde90a
FObermaier <felix.obermaier@netcologne.de>
2013-04-12 20:45:16 +0000

    [Trunk] - Added SharpMap.SpatialReference.Reprojector class, a proxy that uses either   implementation of GeoAPI.SpatialReference.IReprojectorCore - Added NoopReprojector, an implmentation of IReprojectorCore, that does   nothing.
    ToDo here: AffineReprojector, UnitTest
    
    
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C102453

---
+++ SharpMap/SpatialReference/NoopReprojector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using GeoAPI.Features;
+using GeoAPI.Geometries;
+using GeoAPI.SpatialReference;
+
+namespace SharpMap.SpatialReference
+{
+    /// <summary>
+    /// An <see cref="IReprojector"/> implementation, that does not reproject at all
+    /// </summary>
+    public class NoopReprojector : IReprojector
+    {
+        public Coordinate Reproject(Coordinate coordinate, ISpatialReference @from, ISpatialReference to)
+        {
+            return coordinate;
+        }
+
+        public Envelope Reproject(Envelope envelope, ISpatialReference @from, ISpatialReference to)
+        {
+            return envelope;
+        }
+
+        public ICoordinateSequence Reproject(ICoordinateSequence sequence, ISpatialReference @from, ISpatialReference to)
+        {
+            return sequence;
+        }
+
+        public IGeometry Reproject(IGeometry geometry, ISpatialReference @from, ISpatialReference to)
+        {
+            return geometry;
+        }
+
+        public IFeature Reproject(IFeature feature, ISpatialReference @from, ISpatialReference to)
+        {
+            return feature;
+        }
+
+        public IEnumerable<IFeature> Reproject(IEnumerable<IFeature> features, ISpatialReference @from, ISpatialReference to)
+        {
+            return features;
+        }
+    }
+}
---
+++ SharpMap/SpatialReference/Reprojector.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using GeoAPI;
+using GeoAPI.Features;
+using GeoAPI.Geometries;
+using GeoAPI.SpatialReference;
+using GeoAPI.Utilities;
+
+namespace SharpMap.SpatialReference
+{
+    /// <summary>
+    /// Reprojector proxy class used and accessed within SharpMap
+    /// </summary>
+    public class Reprojector : IReprojector
+    {
+        /// <summary>
+        /// The <see cref="IReprojector"/> implementation used internally
+        /// </summary>
+        private readonly IReprojector _instance;
+
+        private readonly ThreadSafeStore<ISpatialReference, IGeometryFactory> _factories =
+            new ThreadSafeStore<ISpatialReference, IGeometryFactory>(CreateGeometryFactory);
+
+        private IGeometryFactory GetFactory(ISpatialReference spatialReference)
+        {
+            return _factories.Get(spatialReference);
+        }
+
+        private static volatile int _newId = 1000000;
+
+        private static IGeometryFactory CreateGeometryFactory(ISpatialReference spatialReference)
+        {
+            var split = spatialReference.Oid.Split(new[] { ':' });
+            int srid;
+            if (split.Length < 2)
+            {
+                srid = ++_newId;
+            }
+            else
+            {
+                if (!(Int32.TryParse(split[1], NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out srid)))
+                    srid = ++_newId;
+            }
+
+            return GeometryServiceProvider.Instance.CreateGeometryFactory(srid);
+        }
+
+
+        /// <summary>
+        /// Creates an instance of this class, assigning a <see cref="NoopReprojector"/>
+        /// </summary>
+        public Reprojector()
+            :this(new NoopReprojector())
+        {}
+
+        /// <summary>
+        /// Creates an instance of this class, that uses <paramref name="instance"/> <see cref="IReprojector"/> internally.
+        /// </summary>
+        /// <param name="instance"></param>
+        public Reprojector(IReprojector instance)
+        {
+            _instance = instance;
+        }
+
+        public Coordinate Reproject(Coordinate point, ISpatialReference from, ISpatialReference to)
+        {
+            return _instance.Reproject(point, from, to);
+        }
+
+        public Envelope Reproject(Envelope envelope, ISpatialReference @from, ISpatialReference to)
+        {
+            return _instance.Reproject(envelope, from, to);
+        }
+
+        public ICoordinateSequence Reproject(ICoordinateSequence sequence, ISpatialReference @from, ISpatialReference to)
+        {
+            return _instance.Reproject(sequence, from, to);
+        }
+
+
+        public IGeometry Reproject(IGeometry geometry, ISpatialReference @from, ISpatialReference to)
+        {
+            var factory = GetFactory(to);
+
+            switch (geometry.OgcGeometryType)
+            {
+                case OgcGeometryType.Point:
+                    return ReprojectPoint(factory, (IPoint)geometry, @from, to);
+
+                case OgcGeometryType.LineString:
+                    return ReprojectLineString(factory, (ILineString)geometry, @from, to);
+
+                case OgcGeometryType.Polygon:
+                    return ReprojectPolygon(factory, (IPolygon)geometry, @from, to);
+
+                default:
+                    var lst = new List<IGeometry>(geometry.NumGeometries);
+                    for (var i = 0; i < geometry.NumGeometries; i++)
+                    {
+                        lst.Add(Reproject(geometry.GetGeometryN(i), from, to));
+                    }
+                    return factory.BuildGeometry(lst);
+            }
+        }
+
+        private IPoint ReprojectPoint(IGeometryFactory factory, IPoint point, ISpatialReference @from,
+                                      ISpatialReference to)
+        {
+            return factory.CreatePoint(Reproject(point.CoordinateSequence, from, to));
+        }
+
+        private ILineString ReprojectLineString(IGeometryFactory factory, ILineString line, ISpatialReference @from,
+                                                  ISpatialReference to, bool lineString = true)
+        {
+            return lineString
+                       ? factory.CreateLineString(Reproject(line.CoordinateSequence, from, to))
+                       : factory.CreateLinearRing(Reproject(line.CoordinateSequence, from, to));
+        }
+
+        private IPolygon ReprojectPolygon(IGeometryFactory factory, IPolygon polygon, ISpatialReference @from,
+                                                 ISpatialReference to)
+        {
+            var exterior = (ILinearRing)ReprojectLineString(factory, polygon.ExteriorRing, from, to, false);
+            var interior = new ILinearRing[polygon.NumInteriorRings];
+            for (var i = 0; i < polygon.NumInteriorRings; i++)
+            {
+                interior[i] = (ILinearRing)ReprojectLineString(factory, polygon.GetInteriorRingN(i), from, to, false);
+            }
+
+            return factory.CreatePolygon(exterior, interior);
+        }
+
+        public IFeature Reproject(IFeature feature, ISpatialReference @from, ISpatialReference to)
+        {
+            feature.Geometry = Reproject(feature.Geometry, @from, to);
+            return feature;
+        }
+
+        public IEnumerable<IFeature> Reproject(IEnumerable<IFeature> features, ISpatialReference @from, ISpatialReference to)
+        {
+            foreach (var feature in features)
+            {
+                yield return Reproject(feature, @from, @to);
+            }
+        }
+    }
+}
