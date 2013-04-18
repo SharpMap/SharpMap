5c96e89e2053309c4eea0ce6568e7ac47617919e
FObermaier <felix.obermaier@netcologne.de>
2013-04-18 08:31:41 +0000

    [Trunk] - added test project for "work in progress" features
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C102517

---
+++ SharpMap.Tests/Features/FeatureTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace SharpMap.Features
+{
+    [TestFixture]
+    public class FeatureTest
+    {
+        private readonly FeatureFactory _featureFactory;
+        private readonly FeatureCollection _featureCollection;
+
+        public FeatureTest()
+        {
+            _featureFactory = new FeatureFactory(
+                NetTopologySuite.NtsGeometryServices.Instance.CreateGeometryFactory(4326),
+                "Attribut1", "Attribut2", "Attribut3");
+
+            _featureCollection = new FeatureCollection(_featureFactory);
+        }
+
+        [Test]
+        public void TestCreate()
+        {
+            var f = _featureCollection.Factory.Create();
+            
+            Assert.IsTrue(f.Geometry == null);
+            Assert.IsTrue(f.Attributes != null);
+            Assert.IsTrue(f.Oid == -1);
+            Assert.IsTrue((int)f.Attributes[0] == -1);
+            
+            Assert.IsTrue(f.Attributes[2] == null);
+            Assert.IsTrue(f.Attributes["Attribut2"] == null);
+
+            f.Attributes[2] = "Test";
+            Assert.IsTrue((string)f.Attributes[2] == "Test");
+            Assert.IsTrue((string)f.Attributes["Attribut2"] == "Test");
+        }
+    }
+}
---
+++ SharpMap.Tests/SpatialReference/SpatialReferenceBaseTest.cs
@@ -0,0 +1,58 @@
+﻿using GeoAPI.Geometries;
+using GeoAPI.SpatialReference;
+using NUnit.Framework;
+
+namespace SharpMap.SpatialReference
+{
+    [TestFixture]
+    public abstract class SpatialReferenceBaseTest
+    {
+        private readonly Reprojector _reprojector;
+        private readonly ISpatialReference _fromSr, _toSr;
+
+        private readonly Coordinate _c0 = new Coordinate();
+
+        protected SpatialReferenceBaseTest(IReprojectorCore core, ISpatialReference @fromSr, ISpatialReference toSr)
+        {
+            _reprojector = new Reprojector(core);
+            _fromSr = @fromSr;
+            _toSr = toSr;
+        }
+
+        [Test]
+        public void TestCoordinate()
+        {
+            Coordinate c1 = null, c2 = null;
+            Assert.DoesNotThrow(() => c1 = _reprojector.Reproject(_c0, _fromSr, _toSr));
+            Assert.DoesNotThrow(() => c2 = _reprojector.Reproject(c1, _fromSr, _toSr));
+            //Assert.AreNotEqual(_c0, c1);
+            //Assert.AreNotEqual(c1, c2);
+            Assert.AreEqual(0d, c2.Distance(_c0), 1.0E-7);
+        }
+
+
+    }
+
+    public class DotSpatialSpatialRefernceTest : SpatialReferenceBaseTest
+    {
+        private static readonly DotSpatialProjectionsSpatialReferenceFactory Factory =
+            new DotSpatialProjectionsSpatialReferenceFactory();
+        
+        public DotSpatialSpatialRefernceTest()
+            :base(new DotSpatialReprojector(), Factory.Create("EPSG:4326"), Factory.Create("EPSG:3758"))
+        {
+        }
+    }
+
+    public class ProjNetSpatialRefernceTest : SpatialReferenceBaseTest
+    {
+        private static readonly ProjNetSpatialReferenceFactory Factory =
+            new ProjNetSpatialReferenceFactory();
+
+        public ProjNetSpatialRefernceTest()
+            : base(new ProjNetReprojector(), Factory.Create("EPSG:4326"), Factory.Create("EPSG:3758"))
+        {
+        }
+    }
+
+}
