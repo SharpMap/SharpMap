030ebf37c4e604e31a13fd197ebc66e2269aa0c8
DGuidi <diegoguidi@gmail.com>
2013-07-15 13:07:13 +0000

    added missing files, again. sorry.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Branches/1.0_WXS_Refactoring;C103401

---
+++ SharpMap/Utilities/ICacheUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SharpMap.Utilities.SpatialIndexing;
+
+namespace SharpMap.Utilities
+{
+    public interface ICacheUtility
+    {
+        bool IsWebContext { get; }
+        bool TryGetValue(string s, out QuadTree quadTree);
+        void TryAddValue(string s, QuadTree quadTree, TimeSpan fromDays);
+    }
+}
---
+++ SharpMap/Utilities/NullCacheUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SharpMap.Utilities.SpatialIndexing;
+
+namespace SharpMap.Utilities
+{
+    internal class NullCacheUtility : ICacheUtility
+    {
+        public bool IsWebContext { get { return false; } }
+
+        public bool TryGetValue(string s, out QuadTree quadTree)
+        {
+            quadTree = null;
+            return false;
+        }
+
+        public void TryAddValue(string s, QuadTree quadTree, TimeSpan fromDays) { }
+    }
+}
