b1e3b84ab3e65b4dd44dea09ad5ab7f6fc8d2a36
FObermaier <felix.obermaier@netcologne.de>
2013-07-17 06:04:23 +0000

    [Branches/1.0] - Updating to latest GeoAPI/NTS unsigned versions - removing binding redirects - Updating to latest BruTile 0.7.4.1 package. Unfortionatly the version info says it is 0.7.3 but it is 0.7.4.1 - Updating SQLite to 1.0.86 - Adding UnitTest for TileLayerIssue
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Branches/1.0;C103422

---
+++ UnitTests/Layers/TileLayerIssues.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using BruTile.Web;
+using GeoAPI.Geometries;
+using NUnit.Framework;
+using SharpMap.Layers;
+
+namespace UnitTests.Layers
+{
+    [TestFixture]
+    public class TileLayerIssues
+    {
+        private string _fileCacheRoot;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _fileCacheRoot = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "BruTileFileCache");
+        }
+
+        [Test, Ignore("Could not reproduce issue"), Description("Map.GetMap() returns incomplete image")]
+        public void TestIncompleteImage()
+        {
+            using (var map = new SharpMap.Map(new Size(2500, 2500)))
+            {
+                map.BackColor = Color.Magenta;
+                var br = new BingRequest(BingRequest.UrlBing, "", BingMapType.Hybrid);
+                var bts = new BingTileSource(br);
+                var tl = new TileLayer(bts, "TileLayer - " + BingMapType.Hybrid.ToString(), Color.Transparent, true,
+                                       System.IO.Path.Combine(_fileCacheRoot, "BingStaging"));
+                map.Layers.Add(tl);
+                
+                map.ZoomToBox(new Envelope(829384.331338522, 837200.785470394, 7068020.31417922, 7072526.73926545)
+                            /*new Envelope(-239839.49199841652, 78451.759683380573, -37033.0152981899, 106723.52879865949)*/);
+                using (var image = map.GetMap())
+                {
+                    image.Save("TestIncompleteImage.png", ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
