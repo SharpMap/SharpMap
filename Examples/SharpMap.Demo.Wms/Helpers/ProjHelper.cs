c5d1a0841c14f227994a55c18d118c0d1e370a91
DGuidi <diegoguidi@gmail.com>
2013-10-24 07:04:51 +0000

    fixed SharpMap.Demo.Wms project to use ProjNet helper classes, as suggested here: https://sharpmap.codeplex.com/discussions/242238
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Trunk;C104305

--- Examples/SharpMap.Demo.Wms/Helpers/ProjHelper.cs
+++ Examples/SharpMap.Demo.Wms/Helpers/ProjHelper.cs
@@ -1,80 +1,25 @@
-﻿namespace SharpMap.Demo.Wms.Helpers
+﻿using GeoAPI.CoordinateSystems;
+using GeoAPI.CoordinateSystems.Transformations;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+
+namespace SharpMap.Demo.Wms.Helpers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Drawing.Drawing2D;
-
-    using GeoAPI.CoordinateSystems;
-    using ProjNet.CoordinateSystems;
-    using GeoAPI.CoordinateSystems.Transformations;
-    using ProjNet.CoordinateSystems.Transformations;
-
-    using SharpMap.Layers;
-    using SharpMap.Rendering;
-
     public static class ProjHelper
     {
+        private static readonly ICoordinateTransformation Transformation;
+
+        static ProjHelper()
+        {
+            CoordinateTransformationFactory factory = new CoordinateTransformationFactory();
+            IGeographicCoordinateSystem wgs84 = GeographicCoordinateSystem.WGS84;
+            IProjectedCoordinateSystem mercator = ProjectedCoordinateSystem.WebMercator;
+            Transformation = factory.CreateFromCoordinateSystems(wgs84, mercator);
+        }
+
         public static ICoordinateTransformation LatLonToGoogle()
-        {
-            CoordinateSystemFactory csFac = new CoordinateSystemFactory();
-            CoordinateTransformationFactory ctFac = new CoordinateTransformationFactory();
-            IGeographicCoordinateSystem sourceCs = csFac.CreateGeographicCoordinateSystem(
-                "WGS 84",
-                AngularUnit.Degrees,
-                HorizontalDatum.WGS84,
-                PrimeMeridian.Greenwich,
-                new AxisInfo("north", AxisOrientationEnum.North),
-                new AxisInfo("east", AxisOrientationEnum.East));
-
-            List<ProjectionParameter> parameters = new List<ProjectionParameter>
-            {
-                new ProjectionParameter("semi_major", 6378137.0),
-                new ProjectionParameter("semi_minor", 6378137.0),
-                new ProjectionParameter("latitude_of_origin", 0.0),
-                new ProjectionParameter("central_meridian", 0.0),
-                new ProjectionParameter("scale_factor", 1.0),
-                new ProjectionParameter("false_easting", 0.0),
-                new ProjectionParameter("false_northing", 0.0)
-            };
-            IProjection projection = csFac.CreateProjection("Google Mercator", "mercator_1sp", parameters);
-            IProjectedCoordinateSystem targetCs = csFac.CreateProjectedCoordinateSystem(
-                "Google Mercator",
-                sourceCs,
-                projection,
-                LinearUnit.Metre,
-                new AxisInfo("East", AxisOrientationEnum.East),
-                new AxisInfo("North", AxisOrientationEnum.North));
-            ICoordinateTransformation transformation = ctFac.CreateFromCoordinateSystems(sourceCs, targetCs);
-            return transformation;
+        {            
+            return Transformation;
         }
-    }
-
-    public static class LabelHelper
-    {
-        public static LabelLayer CreateLabelLayer(VectorLayer src, string column)
-        {
-            string name = String.Format("{0}:Labels", src.LayerName);
-            LabelLayer layer = new LabelLayer(name)
-                {
-                    DataSource = src.DataSource,
-                    LabelColumn = column,
-                    LabelFilter = LabelCollisionDetection.ThoroughCollisionDetection,
-                    MultipartGeometryBehaviour = LabelLayer.MultipartGeometryBehaviourEnum.CommonCenter,
-                    MaxVisible = src.MaxVisible,
-                    MinVisible = src.MinVisible,
-                    Style =
-                        {
-                            CollisionDetection = true,
-                            CollisionBuffer = new SizeF(10F, 10F),
-                            Offset = new PointF(0, -5F),
-                            Font = new Font(FontFamily.GenericSansSerif, 12),
-                            Halo = new Pen(Color.White, 2)
-                        },
-                    SmoothingMode = SmoothingMode.HighQuality,
-                    CoordinateTransformation = src.CoordinateTransformation
-                };
-            return layer;
-        }
-    }
+    }    
 }
