139b3d6af683295acef97cc83f818248c4d95128
suryapratap <suryapratap@gmail.com>
2014-01-17 02:42:54 +0000

    Rename WmsException to WmsExceptionHandler and move to Exceptions/WmsExceptionHandler.cs
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Branches/1.0_WXS_Refactoring;C105011

--- SharpMap.Web/Web/Wms/Exceptions/WmsExceptionHandler.cs
+++ SharpMap.Web/Web/Wms/Exceptions/WmsExceptionHandler.cs
@@ -19,12 +19,12 @@ using System;
 using System.Web;
 using SharpMap.Web.Wms.Server;
 
-namespace SharpMap.Web.Wms
+namespace SharpMap.Web.Wms.Exceptions
 {
     /// <summary>
     /// Class for throwing WMS exceptions to client
     /// </summary>
-    public static class WmsException
+    public static class WmsExceptionHandler
     {
         /// <summary>
         /// Throws a <see cref="WmsExceptionCode.NotApplicable"/> WMS excption. The <paramref name="context"/> is used to write the response stream.
@@ -58,65 +58,65 @@ namespace SharpMap.Web.Wms
             context.Write("</ServiceExceptionReport>");
             context.End();
         }
+    }
 
+    /// <summary>
+    /// WMS Exception codes
+    /// </summary>
+    public enum WmsExceptionCode
+    {
         /// <summary>
-        /// WMS Exception codes
+        /// No error code
         /// </summary>
-        public enum WmsExceptionCode
-        {
-            /// <summary>
-            /// No error code
-            /// </summary>
-            NotApplicable,
-            /// <summary>
-            /// Request contains a Format not offered by the server.
-            /// </summary>
-            InvalidFormat,
-            /// <summary>
-            /// Request contains a CRS not offered by the server for one or more of the
-            /// Layers in the request.
-            /// </summary>
-            InvalidCRS,
-            /// <summary>
-            /// GetMap request is for a Layer not offered by the server, or GetFeatureInfo
-            /// request is for a Layer not shown on the map.
-            /// </summary>
-            LayerNotDefined,
-            /// <summary>
-            /// Request is for a Layer in a Style not offered by the server.
-            /// </summary>
-            StyleNotDefined,
-            /// <summary>
-            /// GetFeatureInfo request is applied to a Layer which is not declared queryable.
-            /// </summary>
-            LayerNotQueryable,
-            /// <summary>
-            /// GetFeatureInfo request contains invalid X or Y value.
-            /// </summary>
-            InvalidPoint,
-            /// <summary>
-            /// Value of (optional) UpdateSequence parameter in GetCapabilities request is
-            /// equal to current value of service metadata update sequence number.
-            /// </summary>
-            CurrentUpdateSequence,
-            /// <summary>
-            /// Value of (optional) UpdateSequence parameter in GetCapabilities request is
-            /// greater than current value of service metadata update sequence number.
-            /// </summary>
-            InvalidUpdateSequence,
-            /// <summary>
-            /// Request does not include a sample dimension value, and the server did not
-            /// declare a default value for that dimension.
-            /// </summary>
-            MissingDimensionValue,
-            /// <summary>
-            /// Request contains an invalid sample dimension value.
-            /// </summary>
-            InvalidDimensionValue,
-            /// <summary>
-            /// Request is for an optional operation that is not supported by the server.
-            /// </summary>
-            OperationNotSupported            
-        }
+        NotApplicable,
+        /// <summary>
+        /// Request contains a Format not offered by the server.
+        /// </summary>
+        InvalidFormat,
+        /// <summary>
+        /// Request contains a CRS not offered by the server for one or more of the
+        /// Layers in the request.
+        /// </summary>
+        InvalidCRS,
+        /// <summary>
+        /// GetMap request is for a Layer not offered by the server, or GetFeatureInfo
+        /// request is for a Layer not shown on the map.
+        /// </summary>
+        LayerNotDefined,
+        /// <summary>
+        /// Request is for a Layer in a Style not offered by the server.
+        /// </summary>
+        StyleNotDefined,
+        /// <summary>
+        /// GetFeatureInfo request is applied to a Layer which is not declared queryable.
+        /// </summary>
+        LayerNotQueryable,
+        /// <summary>
+        /// GetFeatureInfo request contains invalid X or Y value.
+        /// </summary>
+        InvalidPoint,
+        /// <summary>
+        /// Value of (optional) UpdateSequence parameter in GetCapabilities request is
+        /// equal to current value of service metadata update sequence number.
+        /// </summary>
+        CurrentUpdateSequence,
+        /// <summary>
+        /// Value of (optional) UpdateSequence parameter in GetCapabilities request is
+        /// greater than current value of service metadata update sequence number.
+        /// </summary>
+        InvalidUpdateSequence,
+        /// <summary>
+        /// Request does not include a sample dimension value, and the server did not
+        /// declare a default value for that dimension.
+        /// </summary>
+        MissingDimensionValue,
+        /// <summary>
+        /// Request contains an invalid sample dimension value.
+        /// </summary>
+        InvalidDimensionValue,
+        /// <summary>
+        /// Request is for an optional operation that is not supported by the server.
+        /// </summary>
+        OperationNotSupported
     }
 }
