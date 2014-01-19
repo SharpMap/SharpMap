73ce1fe937068bc1f2a607f7bcd8a138895c7ff0
suryapratap <suryapratap@gmail.com>
2014-01-19 06:31:20 +0000

    Refactor of GetFeatureInfo
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Branches/1.0_WXS_Refactoring;C105031

--- SharpMap.Web/Web/Wms/Server/Handlers/GetFeatureInfo.cs
+++ SharpMap.Web/Web/Wms/Server/Handlers/GetFeatureInfo.cs
@@ -136,92 +136,94 @@ namespace SharpMap.Web.Wms.Server.Handlers
         /// <returns>Plain text string with featureinfo results</returns>
         private string CreateFeatureInfoPlain(Map map, string[] requestedLayers, float x, float y, int featureCount, string cqlFilter, int pixelSensitivity, WmsServer.InterSectDelegate intersectDelegate)
         {
-            string vstr = "GetFeatureInfo results: \n";
+            StringBuilder vstr = new StringBuilder("GetFeatureInfo results: \n");
             foreach (string requestLayer in requestedLayers)
             {
-                bool found = false;
-                foreach (ILayer mapLayer in map.Layers)
+                ICanQueryLayer queryLayer = GetQueryLayer(map, requestLayer);
+
+                FeatureDataSet fds;
+                // at this point queryLayer should have a non null value
+                // check if the layer can be queried and if there is any data
+                if (queryLayer.IsQueryEnabled && TryGetData(map, x, y, pixelSensitivity, intersectDelegate, queryLayer, cqlFilter, out fds))
                 {
-                    if (String.Equals(mapLayer.LayerName, requestLayer,
-                        StringComparison.InvariantCultureIgnoreCase))
+                    //if featurecount < fds...count, select smallest bbox, because most likely to be clicked
+                    vstr.AppendFormat("\n Layer: '{0}'\n Featureinfo:\n", requestLayer);
+                    int[] keys = new int[fds.Tables[0].Rows.Count];
+                    double[] area = new double[fds.Tables[0].Rows.Count];
+                    for (int l = 0; l < fds.Tables[0].Rows.Count; l++)
                     {
-                        found = true;
-                        ICanQueryLayer queryLayer = mapLayer as ICanQueryLayer;
-                        if (queryLayer == null || !queryLayer.IsQueryEnabled) continue;
-
-                        float queryBoxMinX = x - pixelSensitivity;
-                        float queryBoxMinY = y - pixelSensitivity;
-                        float queryBoxMaxX = x + pixelSensitivity;
-                        float queryBoxMaxY = y + pixelSensitivity;
-
-                        Coordinate minXY = map.ImageToWorld(new PointF(queryBoxMinX, queryBoxMinY));
-                        Coordinate maxXY = map.ImageToWorld(new PointF(queryBoxMaxX, queryBoxMaxY));
-                        Envelope queryBox = new Envelope(minXY, maxXY);
-                        FeatureDataSet fds = new FeatureDataSet();
-                        queryLayer.ExecuteIntersectionQuery(queryBox, fds);
-
-                        if (intersectDelegate != null)
+                        FeatureDataRow fdr = (FeatureDataRow)fds.Tables[0].Rows[l];
+                        area[l] = fdr.Geometry.EnvelopeInternal.Area;
+                        keys[l] = l;
+                    }
+                    Array.Sort(area, keys);
+                    if (fds.Tables[0].Rows.Count < featureCount)
+                    {
+                        featureCount = fds.Tables[0].Rows.Count;
+                    }
+                    for (int k = 0; k < featureCount; k++)
+                    {
+                        for (int j = 0; j < fds.Tables[0].Rows[keys[k]].ItemArray.Length; j++)
                         {
-                            fds.Tables[0] = intersectDelegate(fds.Tables[0], queryBox);
+                            vstr.AppendFormat(" '{0}'", fds.Tables[0].Rows[keys[k]].ItemArray[j]);
                         }
-                        if (fds.Tables.Count == 0)
-                        {
-                            vstr = vstr + "\nSearch returned no results on layer: " + requestLayer;
-                        }
-                        else
-                        {
-                            if (fds.Tables[0].Rows.Count == 0)
-                            {
-                                vstr = vstr + "\nSearch returned no results on layer: " + requestLayer + " ";
-                            }
-                            else
-                            {
-                                //filter the rows with the CQLFilter if one is provided
-                                if (cqlFilter != null)
-                                {
-                                    for (int i = fds.Tables[0].Rows.Count - 1; i >= 0; i--)
-                                    {
-                                        if (!CqlFilter((FeatureDataRow)fds.Tables[0].Rows[i], cqlFilter))
-                                        {
-                                            fds.Tables[0].Rows.RemoveAt(i);
-                                        }
-                                    }
-                                }
-                                //if featurecount < fds...count, select smallest bbox, because most likely to be clicked
-                                vstr = vstr + "\n Layer: '" + requestLayer + "'\n Featureinfo:\n";
-                                int[] keys = new int[fds.Tables[0].Rows.Count];
-                                double[] area = new double[fds.Tables[0].Rows.Count];
-                                for (int l = 0; l < fds.Tables[0].Rows.Count; l++)
-                                {
-                                    FeatureDataRow fdr = (FeatureDataRow)fds.Tables[0].Rows[l];
-                                    area[l] = fdr.Geometry.EnvelopeInternal.Area;
-                                    keys[l] = l;
-                                }
-                                Array.Sort(area, keys);
-                                if (fds.Tables[0].Rows.Count < featureCount)
-                                {
-                                    featureCount = fds.Tables[0].Rows.Count;
-                                }
-                                for (int k = 0; k < featureCount; k++)
-                                {
-                                    for (int j = 0; j < fds.Tables[0].Rows[keys[k]].ItemArray.Length; j++)
-                                    {
-                                        vstr = vstr + " '" + fds.Tables[0].Rows[keys[k]].ItemArray[j] + "'";
-                                    }
-                                    if ((k + 1) < featureCount)
-                                        vstr = vstr + ",\n";
-                                }
-                            }
-                        }
-
+                        if ((k + 1) < featureCount)
+                            vstr.Append(",\n");
                     }
                 }
-                if (found == false)
+                else
                 {
-                    throw new WmsLayerNotDefinedException(requestLayer);
+                    vstr.AppendFormat("\nSearch returned no results on layer: {0}", requestLayer);
+                }
+
+            }
+            return vstr.ToString();
+        }
+
+        private ICanQueryLayer GetQueryLayer(Map map, string requestLayer)
+        {
+            foreach (ILayer mapLayer in map.Layers)
+            {
+                if (!((String.Equals(mapLayer.LayerName, requestLayer, StringComparison.InvariantCultureIgnoreCase))))
+                    continue;
+
+                return mapLayer as ICanQueryLayer;
+            }
+            throw new WmsLayerNotDefinedException(requestLayer);
+        }
+
+        private bool TryGetData(Map map, float x, float y, int pixelSensitivity, WmsServer.InterSectDelegate intersectDelegate, ICanQueryLayer queryLayer, string cqlFilter, out FeatureDataSet fds)
+        {
+
+            float queryBoxMinX = x - pixelSensitivity;
+            float queryBoxMinY = y - pixelSensitivity;
+            float queryBoxMaxX = x + pixelSensitivity;
+            float queryBoxMaxY = y + pixelSensitivity;
+
+            Coordinate minXY = map.ImageToWorld(new PointF(queryBoxMinX, queryBoxMinY));
+            Coordinate maxXY = map.ImageToWorld(new PointF(queryBoxMaxX, queryBoxMaxY));
+            Envelope queryBox = new Envelope(minXY, maxXY);
+            fds = new FeatureDataSet();
+            queryLayer.ExecuteIntersectionQuery(queryBox, fds);
+
+            if (intersectDelegate != null)
+            {
+                fds.Tables[0] = intersectDelegate(fds.Tables[0], queryBox);
+            }
+
+            //filter the rows with the CQLFilter if one is provided
+            if (cqlFilter != null)
+            {
+                for (int i = fds.Tables[0].Rows.Count - 1; i >= 0; i--)
+                {
+                    if (!CqlFilter((FeatureDataRow)fds.Tables[0].Rows[i], cqlFilter))
+                    {
+                        fds.Tables[0].Rows.RemoveAt(i);
+                    }
                 }
             }
-            return vstr;
+
+            return fds.Tables.Count > 0 && fds.Tables[0].Rows.Count > 0;
         }
 
         /// <summary>
@@ -241,71 +243,39 @@ namespace SharpMap.Web.Wms.Server.Handlers
             List<GeoJSON> items = new List<GeoJSON>();
             foreach (string requestLayer in requestedLayers)
             {
-                bool found = false;
-                foreach (ILayer mapLayer in map.Layers)
+                ICanQueryLayer queryLayer = GetQueryLayer(map, requestLayer);
+
+                FeatureDataSet fds;
+                // at this point queryLayer should have a non null value
+                // check if the layer can be queried and if there is any data
+                if (queryLayer.IsQueryEnabled && TryGetData(map, x, y, pixelSensitivity, intersectDelegate, queryLayer, cqlFilter, out fds))
                 {
-                    if (String.Equals(mapLayer.LayerName, requestLayer,
-                        StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        found = true;
-                        ICanQueryLayer queryLayer = mapLayer as ICanQueryLayer;
-                        if (queryLayer == null || !queryLayer.IsQueryEnabled) continue;
+                    // maybe this part should go into the TryGetData method
+                    // afterall we are going to use data after applying filter
 
-                        float queryBoxMinX = x - pixelSensitivity;
-                        float queryBoxMinY = y - pixelSensitivity;
-                        float queryBoxMaxX = x + pixelSensitivity;
-                        float queryBoxMaxY = y + pixelSensitivity;
-                        Coordinate minXY = map.ImageToWorld(new PointF(queryBoxMinX, queryBoxMinY));
-                        Coordinate maxXY = map.ImageToWorld(new PointF(queryBoxMaxX, queryBoxMaxY));
-                        Envelope queryBox = new Envelope(minXY, maxXY);
-                        FeatureDataSet fds = new FeatureDataSet();
-                        queryLayer.ExecuteIntersectionQuery(queryBox, fds);
-                        //                        
-                        if (intersectDelegate != null)
-                        {
-                            fds.Tables[0] = intersectDelegate(fds.Tables[0], queryBox);
-                        }
-                        //filter the rows with the CQLFilter if one is provided
-                        if (cqlFilter != null)
-                        {
-                            for (int i = fds.Tables[0].Rows.Count - 1; i >= 0; i--)
-                            {
-                                if (!CqlFilter((FeatureDataRow)fds.Tables[0].Rows[i], cqlFilter))
-                                {
-                                    fds.Tables[0].Rows.RemoveAt(i);
-                                }
-                            }
-                        }
-
-                        IEnumerable<GeoJSON> data = GeoJSONHelper.GetData(fds);
+                    IEnumerable<GeoJSON> data = GeoJSONHelper.GetData(fds);
 #if DotSpatialProjections
                         throw new NotImplementedException();
 #else
-                        // Reproject geometries if needed
-                        IMathTransform transform = null;
-                        if (queryLayer is VectorLayer)
-                        {
-                            ICoordinateTransformation transformation = (queryLayer as VectorLayer).CoordinateTransformation;
-                            transform = transformation == null ? null : transformation.MathTransform;
-                        }
-
-                        if (transform != null)
-                        {
-                            data = data.Select(d =>
-                            {
-                                IGeometry converted = GeometryTransform.TransformGeometry(d.Geometry, transform, map.Factory);
-                                d.SetGeometry(converted);
-                                return d;
-                            });
-                        }
-#endif
-                        items.AddRange(data);
-
+                    // Reproject geometries if needed
+                    IMathTransform transform = null;
+                    if (queryLayer is VectorLayer)
+                    {
+                        ICoordinateTransformation transformation = (queryLayer as VectorLayer).CoordinateTransformation;
+                        transform = transformation == null ? null : transformation.MathTransform;
                     }
-                }
-                if (found == false)
-                {
-                    throw new WmsLayerNotDefinedException(requestLayer);
+
+                    if (transform != null)
+                    {
+                        data = data.Select(d =>
+                        {
+                            IGeometry converted = GeometryTransform.TransformGeometry(d.Geometry, transform, map.Factory);
+                            d.SetGeometry(converted);
+                            return d;
+                        });
+                    }
+#endif
+                    items.AddRange(data);
                 }
             }
             StringWriter writer = new StringWriter();
