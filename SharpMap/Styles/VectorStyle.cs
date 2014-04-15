382a580c3d615b8fade516d6cac51af961b28fdc
becio <spartaco@sgsoftware.it>
2014-04-15 10:35:21 +0000

    [Branches/1.0] bugfix: CreateRandomKnownColor could not be used sequentially
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS01]$/SharpMap/Branches/1.0;C105676

--- SharpMap/Styles/VectorStyle.cs
+++ SharpMap/Styles/VectorStyle.cs
@@ -31,6 +31,8 @@ namespace SharpMap.Styles
     [Serializable]
     public class VectorStyle : Style, ICloneable
     {
+        private static readonly Random _rnd = new Random();
+
         static ILog logger = LogManager.GetLogger(typeof(VectorStyle));
         /// <summary>
         /// Default Symbol
@@ -400,17 +402,16 @@ namespace SharpMap.Styles
         /// <param name="res">The style to randomize</param>
         private static void RandomizePuntalStyle(VectorStyle res)
         {
-            var rnd = new Random();
-            switch (rnd.Next(2))
+            switch (_rnd.Next(2))
             {
                 case 0:
                     res.Symbol = DefaultSymbol;
-                    res.SymbolScale = 0.01f * new Random().Next(80, 200);
+                    res.SymbolScale = 0.01f * _rnd.Next(80, 200);
                     break;
                 case 1:
                     res.Symbol = null;
-                    res.PointColor = new SolidBrush(CreateRandomKnownColor(new Random().Next(67, 256)));
-                    res.PointSize = 0.1f*rnd.Next(5, 20);
+                    res.PointColor = new SolidBrush(CreateRandomKnownColor(_rnd.Next(67, 256)));
+                    res.PointSize = 0.1f * _rnd.Next(5, 20);
                     break;
             }
         }
@@ -421,12 +422,10 @@ namespace SharpMap.Styles
         /// <param name="res">The style to randomize</param>
         private static void RandomizeLinealStyle(VectorStyle res)
         {
-            var rnd = new Random();
-            
-            res.Line = new Pen(CreateRandomKnownColor(rnd.Next(67,256)), rnd.Next(1, 3));
-            res.EnableOutline = rnd.Next(0, 2) == 1;
+            res.Line = new Pen(CreateRandomKnownColor(_rnd.Next(67, 256)), _rnd.Next(1, 3));
+            res.EnableOutline = _rnd.Next(0, 2) == 1;
             if (res.EnableOutline)
-                res.Outline = new Pen(CreateRandomKnownColor(rnd.Next(67, 256)), rnd.Next((int) res.Line.Width, 5));
+                res.Outline = new Pen(CreateRandomKnownColor(_rnd.Next(67, 256)), _rnd.Next((int)res.Line.Width, 5));
         }
 
         /// <summary>
@@ -435,19 +434,18 @@ namespace SharpMap.Styles
         /// <param name="res"></param>
         private static void RandomizePolygonalStyle(VectorStyle res)
         {
-            var rnd = new Random();
-            switch (rnd.Next(3))
+            switch (_rnd.Next(3))
             {
                 case 0:
-                    res.Fill = new SolidBrush(CreateRandomKnownColor(rnd.Next(67, 256)));
+                    res.Fill = new SolidBrush(CreateRandomKnownColor(_rnd.Next(67, 256)));
                     break;
                 case 1:
-                    res.Fill = new HatchBrush((HatchStyle)rnd.Next(0, 53),
-                        CreateRandomKnownColor(), CreateRandomKnownColor(rnd.Next(67,256)));
+                    res.Fill = new HatchBrush((HatchStyle)_rnd.Next(0, 53),
+                        CreateRandomKnownColor(), CreateRandomKnownColor(_rnd.Next(67, 256)));
                     break;
                 case 2:
-                    var alpha = rnd.Next(67, 256);
-                    res.Fill = new LinearGradientBrush(new Point(0, 0), new Point(rnd.Next(5, 10), rnd.Next(5, 10)),
+                    var alpha = _rnd.Next(67, 256);
+                    res.Fill = new LinearGradientBrush(new Point(0, 0), new Point(_rnd.Next(5, 10), _rnd.Next(5, 10)),
                         CreateRandomKnownColor(alpha), CreateRandomKnownColor(alpha));
                     break;
             }
@@ -460,7 +458,7 @@ namespace SharpMap.Styles
         /// <returns></returns>
         public static Color CreateRandomKnownColor(int alpha = 255)
         {
-            var kc = (KnownColor) new Random().Next(28, 168);
+            var kc = (KnownColor)_rnd.Next(28, 168);
             return alpha == 255 
                 ? Color.FromKnownColor(kc) 
                 : Color.FromArgb(alpha, Color.FromKnownColor(kc));
