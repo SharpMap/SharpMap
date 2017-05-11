7dd6e80052c919c734024abb4c520da9b7a506fb
FObermaier <felix.obermaier@netcologne.de>
2017-05-11 14:30:07 +0200

    Implement ICoordinateSystem.GetAxis and ICoordinateSystem.GetUnit to the best of my knowledge (issue #10).

--- SharpMap.CoordinateSystems.DotSpatial/DotSpatialCoordinateSystem.cs
+++ SharpMap.CoordinateSystems.DotSpatial/DotSpatialCoordinateSystem.cs
@@ -1,12 +1,40 @@
-﻿
+﻿/*
+ * Copyright © 2017 - Felix Obermaier, Ingenieurgruppe IVV GmbH & Co. KG
+ * 
+ * This file is part of SharpMap.CoordinateSystems.DotSpatial.
+ *
+ * SharpMap.CoordinateSystems.DotSpatial is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * SharpMap.CoordinateSystems.DotSpatial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SharpMap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ *
+ */
 using System;
 using DotSpatial.Projections;
 using GeoAPI.CoordinateSystems;
 
 namespace SharpMap.CoordinateSystems
 {
+    /// <summary>
+    /// A wrapper around a <see cref="DotSpatial.Projections.ProjectionInfo"/>
+    /// </summary>
     public class DotSpatialCoordinateSystem : ICoordinateSystem
     {
+        private static readonly string[] _latLonNames = { "Longitude", "Latitude" };
+        private static readonly string[] _eastNorthNames = { "East", "North" };
+
+        private static readonly AxisOrientationEnum[] _axisOrientation =
+            {AxisOrientationEnum.East, AxisOrientationEnum.North,};
+
         private readonly ProjectionInfo _projectionInfo;
         private string _wkt;
         private readonly string _alias;
@@ -137,14 +165,109 @@ namespace SharpMap.CoordinateSystems
         /// <returns>Axis info</returns>
         public AxisInfo GetAxis(int dimension)
         {
-            throw new NotSupportedException();
+            return ToAxisInfo(_projectionInfo, dimension);
+        }
+
+        private AxisInfo ToAxisInfo(ProjectionInfo projectionInfo, int dimension)
+        {
+            if (projectionInfo.IsLatLon)
+                return new AxisInfo(_latLonNames[dimension], _axisOrientation[dimension]);
+            return new AxisInfo(_eastNorthNames[dimension], _axisOrientation[dimension]);
         }
 
         /// <summary>Gets units for dimension within coordinate system.</summary>
         public IUnit GetUnits(int dimension)
         {
-            throw new NotSupportedException();
+            return ToUnit(_projectionInfo);
         }
+
+        private static IUnit ToUnit(ProjectionInfo projDescriptor)
+        {
+            if (projDescriptor.IsLatLon)
+                return new AngularUnitWrapper(projDescriptor.GeographicInfo.Unit);
+
+            return new LinearUnitWrapper(projDescriptor.Unit);
+        }
+
+        private abstract class UnitWrapper : IUnit
+        {
+            protected readonly IProjDescriptor Descriptor;
+
+            protected UnitWrapper(IProjDescriptor descriptor)
+            {
+                Descriptor = descriptor;
+            }
+
+            public abstract string Name { get; } 
+
+            public string Authority { get { return "DS"; } }
+            public long AuthorityCode { get { return 20000000; } }
+            public string Alias { get { return string.Empty; } }
+            public string Abbreviation { get { return string.Empty; } }
+            public string Remarks { get { return string.Empty; } }
+            public abstract string WKT { get; }
+
+            public string XML { get { throw new NotSupportedException(); } }
+
+            public abstract bool EqualParams(object obj);
+        }
+
+        private class AngularUnitWrapper : UnitWrapper, IAngularUnit
+        {
+            private AngularUnit Unit
+            {
+                get { return (AngularUnit) Descriptor; }
+            }
+
+            public AngularUnitWrapper(AngularUnit angularUnit)
+                :base(angularUnit)
+            {
+            }
+
+            public override string Name { get { return Unit.Name; } }
+
+            public override string WKT { get { return Unit.ToEsriString(); } }
+
+            public override bool EqualParams(object obj)
+            {
+                if (obj == null) return false;
+                if (obj is AngularUnitWrapper)
+                    return ((AngularUnitWrapper) obj).RadiansPerUnit == RadiansPerUnit;
+                return false;
+            }
+
+
+            public double RadiansPerUnit { get; set; }
+        }
+
+        private class LinearUnitWrapper : UnitWrapper, ILinearUnit
+        {
+            private LinearUnit Unit
+            {
+                get { return (LinearUnit)Descriptor; }
+            }
+
+            public LinearUnitWrapper(LinearUnit linearUnit)
+                : base(linearUnit)
+            {
+            }
+
+            public override string Name { get { return Unit.Name; } }
+
+            public override string WKT { get { return Unit.ToEsriString(); } }
+
+            public override bool EqualParams(object obj)
+            {
+                if (obj == null) return false;
+                if (obj is LinearUnitWrapper)
+                    return ((LinearUnitWrapper)obj).MetersPerUnit == MetersPerUnit;
+                return false;
+            }
+
+
+            public double MetersPerUnit { get; set; }
+        }
+
 
         /// <summary>Dimension of the coordinate system.</summary>
         public int Dimension
