2cf8db1902ae237a6de068c0361ce8eec791d465
terribletim <terribletim@users.noreply.github.com>
2018-07-04 19:13:58 +0700

    Support varying colour of RasterPointSymbolizer for thematic rendering (#102)
    * New prop SymbolColor to support color re-map for thematic rendering
    
    * Apply requested changes: public prop RemapColor and follow naming standards

--- SharpMap/Rendering/Symbolizer/RasterPointSymbolizer.cs
+++ SharpMap/Rendering/Symbolizer/RasterPointSymbolizer.cs
@@ -38,6 +38,9 @@ namespace SharpMap.Rendering.Symbolizer
 
         private float _transparency = 0f;
 
+        private Color _symbolColor = Color.Empty;
+        private Color _remapColor = Color.Empty;
+
         /// <summary>
         /// Releases managed resources
         /// </summary>
@@ -58,23 +61,46 @@ namespace SharpMap.Rendering.Symbolizer
             base.ReleaseManagedResources();
         }
 
+        /// <summary>
+        /// Optional transparency in range 0 (opaque) to 1 (fully transparent).
+        /// </summary>
         public float Transparency
         {
             get { return _transparency; }
             set
             {
+                if (value < 0 || value > 1)
+                    throw new ArgumentOutOfRangeException("Require value from 0 (opaque) to 1 (fully transparent)");
                 _transparency = value;
+                ConstructImageAttributes();
+            }
+        }
 
-                if (_imageAttributes != null)
-                    _imageAttributes.Dispose();
+        /// <summary>
+        /// Optional colour to replace the RemapColor pixels in Symbol. 
+        /// If Transparency is also specified, transparency will replace SymbolColor.Alpha.
+        /// </summary>
+        public Color SymbolColor
+        {
+            get { return _symbolColor; }
+            set
+            {
+                _symbolColor = value;
+                ConstructImageAttributes();
+            }
+        }
 
-                if (_transparency == 0)
-                    return;
-
-                _imageAttributes = new ImageAttributes();
-                var cm = new ColorMatrix();
-                cm.Matrix33 = 1f - _transparency;
-                _imageAttributes.SetColorMatrix(cm);
+        /// <summary>
+        /// Optional colour to be replaced by SymbolColor during re-map.
+        /// Pixels must have an exact match (including RemapColor.Alpha) to be re-mapped.
+        /// </summary>
+        public Color RemapColor
+        {
+            get { return _remapColor; }
+            set
+            {
+                _remapColor = value;
+                ConstructImageAttributes();
             }
         }
 
@@ -84,6 +110,8 @@ namespace SharpMap.Rendering.Symbolizer
             var res = (RasterPointSymbolizer)MemberwiseClone();
             res.Transparency = Transparency;
             res.Symbol = (Image)Symbol.Clone();
+            res.SymbolColor = SymbolColor;
+            res.RemapColor = RemapColor;
 
             return res;
         }
@@ -104,6 +132,41 @@ namespace SharpMap.Rendering.Symbolizer
             }
         }
 
+        /// <summary>
+        /// Construct imageattribute based upon Transparency and/or Color Re-map
+        /// </summary>
+        private void ConstructImageAttributes()
+        {
+            if (_imageAttributes != null)
+                _imageAttributes.Dispose();
+
+            if (Transparency == 0 && (SymbolColor.ToArgb() == RemapColor.ToArgb()))
+                return;
+
+            _imageAttributes = new ImageAttributes();
+
+            if (SymbolColor.ToArgb() != RemapColor.ToArgb())
+            {
+                var cm = new ColorMap[1];
+
+                var a = SymbolColor.A;
+
+                if (Transparency > 0)
+                    a = (byte)(Math.Ceiling(255 * (1F - Transparency)));
+
+                var nc = Color.FromArgb(a, SymbolColor);
+                cm[0] = new ColorMap();
+                cm[0].OldColor = RemapColor;
+                cm[0].NewColor = nc;
+                ImageAttributes.SetRemapTable(cm);
+            }
+            else
+            {
+                var cm = new ColorMatrix();
+                cm.Matrix33 = 1F - _transparency;
+                ImageAttributes.SetColorMatrix(cm);
+            }
+        }
 
         /// <summary>
         /// Gets or sets the Size of the symbol
