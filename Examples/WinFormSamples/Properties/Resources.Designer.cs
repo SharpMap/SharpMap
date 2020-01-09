9206cd8c672d5cff58e403e52132de1bd5bbaec2
Tim C <terribletum@gmail.com>
2020-01-09 19:49:14 +1300

    Add FormInteractiveVectorLayerRendering to WinFormSamples

--- Examples/WinFormSamples/Properties/Resources.Designer.cs
+++ Examples/WinFormSamples/Properties/Resources.Designer.cs
@@ -79,7 +79,19 @@ namespace WinFormSamples.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap vessel_01
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("vessel_01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
